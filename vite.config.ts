897e4ab0767755a0e0d3f47fae7c19c095fd8eb6
Colin Maher <colin.maher@famobi.com>
2024-04-12 10:45:15 +0200

    add folder aliases to Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
+import * as path from "path";
 
 export default defineConfig({
   plugins: [solid()],
   esbuild: {
     legalComments: "none",
+  },
+  resolve: {
+    alias: {
+      assets: path.resolve(__dirname, "src/assets"),
+      components: path.resolve(__dirname, "src/components"),
+      layouts: path.resolve(__dirname, "src/layouts"),
+      pages: path.resolve(__dirname, "src/pages"),
+      styles: path.resolve(__dirname, "src/styles"),
+    },
   },
   css: {
     preprocessorMaxWorkers: true,
